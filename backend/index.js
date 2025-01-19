b8f07eb683d618ae80ffd315c88da688d714c91f
pavankalyan <114129248+pavanhoney630@users.noreply.github.com>
2025-01-19 07:51:38 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use(cors(
   {
-    origin:["https://book-store-mern-stack-frontend-seven.vercel.app/"],
+    origin:["https://book-store-mern-stack-frontend-seven.vercel.app"],
     method:["POST","GET","PUT","DELETE"],
     credentials: true
   }
